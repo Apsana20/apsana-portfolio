a805c265152c7fc78b299144cd16efbcc66e309c
Apsana20 <apsanasa03@gmail.com>
2026-06-26 14:10:05 +0530

    Responsive mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
+
 function Navbar() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
 
-      <div className="logo">
-        APSANA
+      <div className="logo">APSANA</div>
+
+      <div
+        className="menu-icon"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        ☰
       </div>
 
-      <ul className="nav-links">
+      <ul className={menuOpen ? "nav-links active" : "nav-links"}>
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#skills">Skills</a></li>
